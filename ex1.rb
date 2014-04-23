b4764688df8f436d2d36d1a94feb1b0d823f85b3
kmccarthy623 <mccarthy.katie@gmail.com>
2014-04-23 19:45:19 -0400

    Exericise 1- Completed

---
+++ ex1.rb
@@ -0,0 +1,9 @@
+#Exercie 1
+
+puts "Hello World!"
+puts "Hello World"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing"
+puts "I'd much rather 'not'."
+puts 'I "said" do not touch this.' 
