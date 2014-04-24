24b7ca9df83d5ec3deff87942d60cf693d5b1019
kmccarthy623 <mccarthy.katie@gmail.com>
2014-04-24 07:59:42 -0400

    Completed Ex 3 and EC on 3. Changed the title i Ex to reflect Exercise two.

---
+++ ex3.rb
@@ -0,0 +1,44 @@
+#Exercise Three
+
+# + plus  addition
+# - minus subtraction 
+# / slash division 
+# * asterisk multiples 
+# % percent division and returns the remainder 
+# < less-than   asks if the values is less than another value which results in a boolean expression (True or False)
+# > greater-than  asks if the value is greater than another value which results in a boolean expression (True or False)
+# <= less-than-equal  asks if a value is less than or equal to another value which results in a boolean expression
+# >= greater-than-equal asks if a value is greater than or equal to another value which results in a boolean expression 
+
+# Tells the user something 
+puts "I will now count my chickens"
+# Prints Hens as strinf and adds 25 to 30 divided by 6 
+puts "Hens", 25 + 30 / 6 
+# Prints Roosters as a string and does math  
+puts "Roosters", 100 - 25 * 3 % 4
+#Prints a sentence as a string
+puts "Now I will count the eggs:"
+#does lots of math 
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#prints sentence as a string 
+puts "Is it true that 3 + 2 < 5 - 7? "
+# boolean express of true or false
+puts 3 + 2 < 5 - 7 
+# prints a string asking question then adds the numbers 
+puts "What is 3 + 2?", 3 + 2
+# prints a string asking a question then adds the numbers  
+puts "What is 5 - 7?", 5 - 7
+# prints the sentence 
+puts "Oh, that's why it's false."
+#prints the sentence 
+puts "How about some more."
+# prints a sentence then performs a boolean expression  
+puts "Is it greater?" , 5 > -2
+# prints a sentence then performs a boolean expression 
+puts "Is it greater or equal?" , 5 >= -2
+# prints a sentence then performs a boolean expression
+puts "Is it less or equal?" , 5 <= 2
+
+#Extra Credit is in ex3_ec.rb as per the directions 
+
+
---
+++ ex3_ec.rb
@@ -0,0 +1,37 @@
+#extra credit for exercise three
+#no decimals 
+puts 10/3
+puts 12/3
+puts 1550 + 1550 
+puts 7000-5400
+
+#floating point number is numbers with decimal points 
+
+# Tells the user something 
+puts "I will now count my chickens"
+# Prints Hens as strinf and adds 25 to 30 divided by 6 
+puts "Hens", 25.0 + 30.0 / 6.0
+# Prints Roosters as a string and does math  
+puts "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+#Prints a sentence as a string
+puts "Now I will count the eggs:"
+#does lots of math 
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+#prints sentence as a string 
+puts "Is it true that 3 + 2 < 5 - 7? "
+# boolean express of true or false
+puts 3.0 + 2.0 < 5.0 - 7.0 
+# prints a string asking question then adds the numbers 
+puts "What is 3 + 2?", 3.0 + 2.0
+# prints a string asking a question then adds the numbers  
+puts "What is 5 - 7?", 5.0 - 7.0
+# prints the sentence 
+puts "Oh, that's why it's false."
+#prints the sentence 
+puts "How about some more."
+# prints a sentence then performs a boolean expression  
+puts "Is it greater?" , 5.0 > -2.0
+# prints a sentence then performs a boolean expression 
+puts "Is it greater or equal?" , 5.0 >= -2.0
+# prints a sentence then performs a boolean expression
+puts "Is it less or equal?" , 5.0 <= 2.0
