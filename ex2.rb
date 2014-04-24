0f08cc732bea3b7ed6f0dd46eadbfc145fad85b4
kmccarthy623 <mccarthy.katie@gmail.com>
2014-04-23 20:13:01 -0400

    Completed Exercise Two

---
+++ ex2.rb
@@ -0,0 +1,13 @@
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by Ruby. 
+
+puts "I could have code like this." # and the comment after is ignored
+
+# You can also use a commet to "disable" or comment out a piece of code:
+# puts "This won't run."
+puts "This will run."
+
+# Extra Credit
+#1. Yes! the hashtag ignores the typing after it! 
+#2 Mistakes: I mistyped YOu, but corrected it and I forgot the "" in disable. 
+#3 Yes, I forgot the ' in won't!
