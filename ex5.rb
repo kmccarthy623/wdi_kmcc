248f3a06ccccbad3ec54201cf36427acd0c0d77a
kmccarthy623 <mccarthy.katie@gmail.com>
2014-04-24 09:16:52 -0400

    Exercise 5 - still need to finish EC

---
+++ ex5.rb
@@ -0,0 +1,57 @@
+#Exercise 5
+
+my_name = 'Zed A. Shaw'
+my_age = 35 # not a lie
+my_height = 74 #inches
+my_weight = 180 #LBS
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = "Brown"
+
+puts "Lets talk about %s." % my_name
+puts "He's %d inched tall." % my_height
+puts "He's %d pounds heavy." % my_weight
+puts "Actually thats's not too heavy."
+puts "He's got %s eyes and %s hair." % [my_eyes, my_hair]
+puts "His teeth are usually %s depending on the coffee." % my_teeth
+
+# this line is tricky, try to get it exactly right
+puts "If I had %d, %d, and %d I get %d." % [
+  my_age, my_height, my_weight, my_age + my_height + my_weight]
+
+  #extra credit 
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 #inches
+weight = 180 #LBS
+eyes = 'Blue'
+teeth = 'White'
+hair = "Brown"
+
+puts "Lets talk about %s." % name
+puts "He's %d inched tall." % height
+puts "He's %d pounds heavy." % weight
+puts "Actually thats's not too heavy."
+puts "He's got %s eyes and %s hair." % [eyes, hair]
+puts "His teeth are usually %s depending on the coffee." % teeth
+
+# this line is tricky, try to get it exactly right
+puts "If I had %d, %d, and %d I get %d." % [
+  age, height, weight, age + height + weight]
+
+trip = 5 #weeks
+countries = 7 
+cities = 15
+friends_total = 5
+destination = 'Europe'
+friend_1 = "Caitlin"
+friend_2 = "Kiki"
+friend_3 = "El"
+friend_4 = "Juli"
+friend_5 = "Alli"
+puts "I'm heading on a adventure to %s for  %d weeks!" % [destination, trip]
+puts "I'm going to %d different countries and %d different cities" % [countries, cities]
+puts "%s, %s, %s, %s, and %s are all coming to meet me!" % [friend_1, friend_2, friend_3, friend_4, friend_5]
+
+
+
