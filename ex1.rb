34559122cce4d2c714dcb1e95f47901e14abfc1b
kmccarthy623 <mccarthy.katie@gmail.com>
2014-04-23 20:05:06 -0400

    Completing Exercise One

--- ex1.rb
+++ ex1.rb
@@ -1,9 +1,23 @@
-#Exercie 1
-
+#Exercise 1
 puts "Hello World!"
-puts "Hello World"
+puts "Hello World!"
 puts "I like typing this."
 puts "This is fun."
 puts "Yay! Printing"
 puts "I'd much rather 'not'."
 puts 'I "said" do not touch this.' 
+
+#Extra Credit 
+# 1. Add another line 
+puts 'Daivid, you are my hero!'
+# 2. Print only one line 
+puts "Hello World!"
+# puts "Hello World!"
+# puts "I like typing this."
+# puts "This is fun."
+# puts "Yay! Printing"
+# puts "I'd much rather 'not'."
+# puts 'I "said" do not touch this.'
+
+# 3. put a # at the beginning of a line. What does it do? It ignores the code and comments it out.
+
