d3141aa779e3228b3467440b4e20d8a256938e02
kmccarthy623 <mccarthy.katie@gmail.com>
2014-04-24 23:13:38 -0400

    Exercise 10

---
+++ ex10.rb
@@ -0,0 +1,45 @@
+#Exercise Ten 
+
+puts "I am 6'2\" tall. "
+puts 'I am 6\'2" tall.'
+
+tabby_cat = "\tI'm tabbed in"
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat." 
+
+fat_cat = <<MY_HEREDOC
+I'll do a list: 
+\t* Cat food
+\t* Fishes
+\t* Catnip\n\t* Grass
+MY_HEREDOC
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat 
+
+
+#Extra Credit
+# \” – double quote
+# \\ – single backslash
+# \a – bell/alert
+# \b – backspace
+# \r – carriage return
+# \n – newline
+# \s – space
+# \t – tab
+
+
+puts "Everyone is yelling \”Go REDSOX ."
+puts "I was born on 6\\23\\83"
+puts "xyus \bhuihih" 
+puts "xyus \ahuihih" 
+puts "bhuihih\r" 
+# \\ – single backslash
+# \a – bell/alert
+# \b – backspace
+# \r – carriage return
+# \n – newline
+# \s – space
+# \t – tab
