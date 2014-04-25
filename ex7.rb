6630aa666b85652c34661641564370429257569c
kmccarthy623 <mccarthy.katie@gmail.com>
2014-04-24 20:09:59 -0400

    Completed Exercise Seven

---
+++ ex7.rb
@@ -0,0 +1,39 @@
+#Exercise seven 
+#print out the string: Mary had a little lamb
+puts "Mary had a little lamb."
+#print out the string: Its fleece was white as the variable snow
+puts "Its fleece was white as %s. " %'snow'
+#print out the string:  And everwhere that Mary went. 
+puts "And everywhere that Mary went."
+#Prints out the string . 10 times 
+puts "." * 10 #what'd that do? 
+
+# Gives String values to end# variables 
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# notice how we are using print instead of puts here.chage it to puts 
+# and see what happens. #EC-> puts and strings are the same, but print 
+# printed this out as two lines.  Print seems to continue the string on the same line  
+
+puts end1 + end2 + end3 + end4 + end5 + end6 
+puts end7 + end8 + end9 + end10 + end11 + end12
+
+#this just is polite use of the terminal, try removing it 
+puts
+ #removing it is fine- it just give a little more space as it returns an empty line
+
+
+# extra credit
+#1. Adding commets to each line
+#2. writing down your errors---I didn't see any this time around..but I am sure i will have some soon!
