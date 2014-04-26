0788bf0d667b833975e04d791390cfede2a866e0
kmccarthy623 <mccarthy.katie@gmail.com>
2014-04-26 13:08:54 -0400

    Finishing Extra Credit for Exerice 10.

--- ex10.rb
+++ ex10.rb
@@ -31,15 +31,24 @@ puts fat_cat
 # \t – tab
 
 
-puts "Everyone is yelling \”Go REDSOX ."
-puts "I was born on 6\\23\\83"
-puts "xyus \bhuihih" 
-puts "xyus \ahuihih" 
-puts "bhuihih\r" 
-# \\ – single backslash
-# \a – bell/alert
-# \b – backspace
-# \r – carriage return
-# \n – newline
-# \s – space
-# \t – tab
+
+Person1 = "Caitlin"
+Person2 = "Eleanor"
+Person3 = "Jule"
+Person4 = "Alli"
+Team1 = "Red Sox"
+Team2 = "Yankees"
+Team3 = "The Mets"
+Team4 = "Chicago Cubs"
+
+
+puts "Lets go to a baseball game! Here are the list of teams I would like to see:
+ \t#{Team1} 
+ \t#{Team2} 
+ \t#{Team3} 
+ \t#{Team4}"
+puts "#{Person1} only wants to see #{Team1} and #{Team2}. \n#{Person2} will only see #{Team1} and #{Team4}"
+puts "#{Person4} and I are fans of #{Team1} and #{Team3} "
+puts "The weekends we can go are 7\\19 and 8\\19."
+puts "I will be cheering \"Go RED SOX!\"" 
+
